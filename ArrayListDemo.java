6e0626a25ea1007d9c8e763fb4be87e8c085cb52
bhargav0343 <bhargav0343@gmail.com>
2019-02-19 18:32:27 +0530

    hello

---
+++ ArrayListDemo.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import Java.util.*;
+class ArrayListDemo {
+	public static void main(String()args){
+		ArrayList<E> l=new ArrayList();z
+		l.add("A");
+		l.add(10);
+		l.add("A");
+		l.add(null);
+		System.out.println(l);
+		l.remove(2);
+		System.out.println(l);
+		l.add(2,"M");
+		l.add("N)
+		System.out.println(l);
+	}
+
+}
---
+++ bhargav.java
@@ -0,0 +1,5 @@
+class bhargav{
+  public static void main(String args[]){
+    System.out.println("Hai");
+ }
+}
